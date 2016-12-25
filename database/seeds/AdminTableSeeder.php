3e9e45449762f4d06c3fc31094a9591b440af113
Niels Drost <nielsdrost7@gmail.com>
2016-12-25 16:05:31 +0100

    Seeding tables (really basic)

---
+++ database/seeds/AdminTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+
+class AdminTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('123456'),
+        ]);
+    }
+}
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(RolesTableSeeder::class);
+        $this->call(AdminTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
         // $this->call(UsersTableSeeder::class);
     }
 }
